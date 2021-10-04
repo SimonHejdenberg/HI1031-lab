ce3df5306edeee104c8018fe343afac33ba5cb79
SimonHejdenberg <simonhej@kth.se>
2021-10-04 19:42:56 +0200

    Fungerande connect string
    woooo

--- web/src/main/java/App.java
+++ web/src/main/java/App.java
@@ -1,24 +1,19 @@
+package web.src.main.java;
+
 import java.sql.*;
 
 public class App {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
-    public static void main(String[] args) {
-        String connectionUrl =
-                "jdbc:sqlserver://localhost:3306;"
-                        + "database=Lab 1;"
-                        + "user=root@localhost;"
-                        + "password=Ltz7ZC366BPt4MZ;" //Ltz7ZC366BPt4MZ
-                        + "encrypt=true;"
-                        + "trustServerCertificate=false;"
-                        + "loginTimeout=30;";
-
-        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-            // Code here.
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws ClassNotFoundException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lab1", "root", "Ltz7ZC366BPt4MZ");
+            if (con != null) {
+                System.out.println("Connection succesfull!");
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
         }
     }
 }
